9852afbf8b2e24f0af4558b18d82e319f930ff96
Rucareanu Alexandru <rucar.alex@gmail.com>
2016-04-19 16:07:31 +0300

    Some changes to the home view

--- src/main/java/com/mycompany/test/vaadin/Views/HomeView.java
+++ src/main/java/com/mycompany/test/vaadin/Views/HomeView.java
@@ -5,14 +5,19 @@
  */
 package com.mycompany.test.vaadin.Views;
 
+import com.mycompany.test.vaadin.Entities.Models;
+import com.mycompany.test.vaadin.Facades.ModelsFacade;
 import com.mycompany.test.vaadin.UI.TecoMainUi;
 import com.vaadin.cdi.CDIView;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import javax.inject.Inject;
 
 /**
  *
@@ -22,7 +27,13 @@ import com.vaadin.ui.VerticalLayout;
 public class HomeView extends CustomComponent implements View{
 
     public static final String NAME = "home"; 
-    public boolean open = true;
+    
+    @Inject
+    private ModelsFacade modelService;
+    private BeanItemContainer<Models> modelsContainer;
+    private Grid modelsGrid;
+    
+    
     
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
@@ -38,6 +49,20 @@ public class HomeView extends CustomComponent implements View{
         p.setComponentAlignment(l, Alignment.MIDDLE_CENTER);
 
         setCompositionRoot(p);
+    }
+    
+    private void buildLayout() {
+        
+    }
+    
+    private void buildModelsContainer() {
+        modelsContainer = new BeanItemContainer<>(Models.class, modelService.findAll());
+    }
+    
+    private void buildModelsGrid() {
+        modelsGrid = new Grid();
+        modelsGrid.setContainerDataSource(modelsContainer);
+        
     }
     
 }
